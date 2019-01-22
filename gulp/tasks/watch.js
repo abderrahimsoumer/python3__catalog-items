f3820aa411516e1562628af89d8736d8725920c9
remuos <soumer120@gmail.com>
2019-01-22 17:31:44 +0100

    Design login page with HTML + CSS

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app/src"
+    }
+  });
+
+  watch('./app/src/templates/*.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/css/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
+});
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/src/static/css/styles.css')
+    .pipe(browserSync.stream());
+});
+
+
